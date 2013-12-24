073f57102d42b390b93145b722c80d6f8449b24c
Eric Kinkead <me@lavalevel.com>
2013-12-23 21:50:08 -0600

    commit4
    Now working with STRAIGHT LUA from TILED!!!

--- main.lua
+++ main.lua
@@ -8,15 +8,28 @@ display.setDefault ("magTextureFilter", "nearest") -- make it nice and pixeled
 
 -- Some kind of map thing
 --foot = display.newImage("mapTile1.png", 100, 100)
-local myText = display.newText( "Hello TileMap!", 300, 0, native.systemFont, 16 )
-local currentMap = {}
 
 -- Set up the map. Best to NOT do this with hardcoded Numbers, so you can make minimaps, etc.
 
-currentMap.mapWidth = 13 -- This is how many items(rows) are in 1 row of a map.
-currentMap.mapHeigth = 12 -- This is how many down
-currentMap.TileSizeSquare = 20 -- how many pixels a square
 
+myTable = require(".images.TileMaps.16x16start")
+atrace (xinspect(myTable))
+
+print (myTable.layers[1].properties.mapName)
+print (myTable.width)
+print (myTable.height)
+print (myTable.tileheight)
+
+local currentMap = {}
+currentMap.mapWidth = myTable.width -- This is how many items(rows) are in 1 row of a map.
+currentMap.mapHeigth = myTable.height -- This is how many down
+currentMap.TileSizeSquare = myTable.layers[1].properties.width -- how many pixels a square
+-- mapHeigth = 
+
+currentMap.mapData = myTable.layers[1].data
+
+atrace (currentMap.mapData )
+--[[
 currentMap.mapData = {
 
 			2,2,3,1,1,1,1,1,1,1,1,1,2,
@@ -33,8 +46,15 @@ currentMap.mapData = {
 			1,1,1,1,1,1,1,1,1,1,1,1,2,
 
 			}
-
+]]
 -- OK.  We have a map.  Now we draw it...
+local myText1 = display.newText( "MapName: "..  myTable.layers[1].properties.mapName, 330, 20, native.systemFont, 12 )
+myText1 .anchorX = 0 ; myText1.anchorY = 1
+
+local myText2 = display.newText( "MapType: "..  myTable.layers[1].name, 330, 40, native.systemFont, 12 )
+myText2 .anchorX = 0 ; myText2.anchorY = 1
+
+
 
 -- think of Y as Rows, and X as columns
 local row = 0
@@ -42,6 +62,7 @@ local column =0
 local mapLocation = 0
 local myMapGroup = display.newGroup()
 local tileNumber
+local currentTile = {}
 
 local drawX = -10
 local drawY = 0
@@ -55,10 +76,10 @@ for row=1, (currentMap.mapHeigth) do  -- This will start at the top first row an
 					
 				
 				tileNumber = currentMap.mapData [((currentMap.mapWidth*(row-1))+ (column)  ) ] 
-				-- print (currentMap.mapData [((currentMap.mapWidth*(row-1))+ (column)  ) ] )
+			-- 	print (currentMap.mapData [((currentMap.mapWidth*(row-1))+ (column)  ) ] )
 				io.write (tileNumber) -- this will print the column in the Terminal
 				--Now that we have the correct tile, lets append the correct filename for the tile.
-				--currentTile = display.newImage( "/images/mapTile1.png" )
+				
 				currentTile = display.newImage( "images/mapTile" .. tileNumber .. ".png" )
 				currentTile.x = 10 
 				currentTile.y = 10 
