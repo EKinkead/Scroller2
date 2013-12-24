0ed6fc191d9985bc7350958001ba5f70930e8a47
Eric Kinkead <me@lavalevel.com>
2013-12-24 00:22:46 -0600

    5th one up
    Reads maps of various sizes

--- main.lua
+++ main.lua
@@ -12,9 +12,12 @@ display.setDefault ("magTextureFilter", "nearest") -- make it nice and pixeled
 -- Set up the map. Best to NOT do this with hardcoded Numbers, so you can make minimaps, etc.
 
 
-myTable = require(".images.TileMaps.16x16start")
+--myTable = require("TileMaps.mapLarge") - testing a large map
+myTable = require("TileMaps.16x16start")
 atrace (xinspect(myTable))
 
+
+-- check out some of these variables for the map I am loading
 print (myTable.layers[1].properties.mapName)
 print (myTable.width)
 print (myTable.height)
@@ -23,12 +26,14 @@ print (myTable.tileheight)
 local currentMap = {}
 currentMap.mapWidth = myTable.width -- This is how many items(rows) are in 1 row of a map.
 currentMap.mapHeigth = myTable.height -- This is how many down
-currentMap.TileSizeSquare = myTable.layers[1].properties.width -- how many pixels a square
+currentMap.TileSizeSquared = myTable.width -- how many pixels a square
 -- mapHeigth = 
+
+-- print (currentMap.TileSizeSquared)
 
 currentMap.mapData = myTable.layers[1].data
 
-atrace (currentMap.mapData )
+-- atrace (currentMap.mapData )
 --[[
 currentMap.mapData = {
 
@@ -49,7 +54,7 @@ currentMap.mapData = {
 ]]
 -- OK.  We have a map.  Now we draw it...
 local myText1 = display.newText( "MapName: "..  myTable.layers[1].properties.mapName, 330, 20, native.systemFont, 12 )
-myText1 .anchorX = 0 ; myText1.anchorY = 1
+myText1.anchorX = 0 ; myText1.anchorY = 1
 
 local myText2 = display.newText( "MapType: "..  myTable.layers[1].name, 330, 40, native.systemFont, 12 )
 myText2 .anchorX = 0 ; myText2.anchorY = 1
@@ -64,7 +69,7 @@ local myMapGroup = display.newGroup()
 local tileNumber
 local currentTile = {}
 
-local drawX = -10
+local drawX = 0
 local drawY = 0
 
 for row=1, (currentMap.mapHeigth) do  -- This will start at the top first row and work down
@@ -72,7 +77,7 @@ for row=1, (currentMap.mapHeigth) do  -- This will start at the top first row an
 
 			for column=1, (currentMap.mapWidth) do
 				-- mapLocation = mapLocation + 1 
-				drawX = drawX+ 20
+				drawX = drawX+ currentMap.TileSizeSquared
 					
 				
 				tileNumber = currentMap.mapData [((currentMap.mapWidth*(row-1))+ (column)  ) ] 
@@ -81,16 +86,17 @@ for row=1, (currentMap.mapHeigth) do  -- This will start at the top first row an
 				--Now that we have the correct tile, lets append the correct filename for the tile.
 				
 				currentTile = display.newImage( "images/mapTile" .. tileNumber .. ".png" )
-				currentTile.x = 10 
-				currentTile.y = 10 
+				currentTile.anchorX = 0 ; currentTile.anchorY = 0
+				currentTile.x = 0 
+				currentTile.y = 0 
 				
 
-				currentTile.x = currentTile.x + (column-1) * 20  -- more MAGIC STEP. since we set that first row to ZERO, if you are on it it won't multiply an extra 20
+				currentTile.x = currentTile.x + (column-1) * currentMap.TileSizeSquared  -- more MAGIC STEP. since we set that first row to ZERO, if you are on it it won't multiply an extra 20
  				currentTile.y = currentTile.y + drawY
 
 			end
 
-			drawY = drawY + 20 -- this moves the tiledrawn down 1 on the app screen
+			drawY = drawY + currentMap.TileSizeSquared -- this moves the tiledrawn down 1 on the app screen
 
 			io.write ("\n") -- This moves the terminal to the next line
 
